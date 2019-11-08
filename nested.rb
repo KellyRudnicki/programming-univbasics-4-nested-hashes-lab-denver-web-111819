7cea2d6b2d17cc5c7e1f29e63679540bf093699e
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-08 05:36:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,7 +136,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:alan_kay][:known_for] = "Assembly"
+     programmer_hash[:alan_kay][:languages] = "Assembly"
      return programmer_hash
 
 end
